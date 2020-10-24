db4064ad5e279be4bc68c42920645777525bae83
OlgaKor23 <work.olga.koryshkina@gmail.com>
2020-10-24 13:55:37 +0300

    reuploaded module 12

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,1 @@
+function fetchCountries(searchQuery) {    const url = `https://restcountries.eu/rest/v2/name/${searchQuery}`;    return fetch(url)        .then(response => response.json())        .catch(error => consol.log(error));}export default fetchCountries;
---
+++ src/js/refs.js
@@ -0,0 +1,1 @@
+const refs = {    searchCountry: document.querySelector('.js-search'),    containerCountry: document.querySelector('.js-countries'),};export default refs;
